10ff552b60555867f5c0fcc6a87f598af81614bf
John Knotts <jhnnyk@gmail.com>
2024-07-19 12:23:28 -0600

    composable useCurrencify

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -5,7 +5,9 @@
         <q-item v-for="entry in entries" :key="entry.id">
           <q-item-section>{{ entry.name }}</q-item-section>
 
-          <q-item-section side>{{ currencify(entry.amount) }}</q-item-section>
+          <q-item-section side>
+            {{ useCurrencify(entry.amount) }}
+          </q-item-section>
         </q-item>
       </q-list>
     </div>
@@ -14,50 +16,30 @@
 
 <script setup>
 // imports
-import { ref } from "vue"
+import { ref } from 'vue'
+import { useCurrencify } from 'src/use/useCurrencify'
 
 // entries
 const entries = ref([
   {
-    id: "id1",
-    name: "Salary",
+    id: 'id1',
+    name: 'Salary',
     amount: 4999.99,
   },
   {
-    id: "id2",
-    name: "Rent",
+    id: 'id2',
+    name: 'Rent',
     amount: -999,
   },
   {
-    id: "id3",
-    name: "Phone",
+    id: 'id3',
+    name: 'Phone',
     amount: -14.99,
   },
   {
-    id: "id4",
-    name: "Unknown",
+    id: 'id4',
+    name: 'Unknown',
     amount: 0,
   },
 ])
-
-// currencify
-function currencify(amount) {
-  // format: "+ $ 4,999.99" | "- $ 999.00"
-  let posNegSymbol = ""
-  if (amount > 0) {
-    posNegSymbol = "+"
-  } else if (amount < 0) {
-    posNegSymbol = "-"
-  }
-
-  const currencySymbol = "$"
-  const amountPositive = Math.abs(amount)
-
-  const amountFormatted = amountPositive.toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
-
-  return `${posNegSymbol} ${currencySymbol} ${amountFormatted}`
-}
 </script>
---
+++ src/use/useCurrencify.js
@@ -0,0 +1,19 @@
+export function useCurrencify(amount) {
+  // format: "+ $ 4,999.99" | "- $ 999.00"
+  let posNegSymbol = ''
+  if (amount > 0) {
+    posNegSymbol = '+'
+  } else if (amount < 0) {
+    posNegSymbol = '-'
+  }
+
+  const currencySymbol = '$'
+  const amountPositive = Math.abs(amount)
+
+  const amountFormatted = amountPositive.toLocaleString('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+
+  return `${posNegSymbol} ${currencySymbol} ${amountFormatted}`
+}
