d24b4691ae8ffbe8d8ea398854b31930da55dbe2
John Knotts <jhnnyk@gmail.com>
2024-07-23 15:34:18 -0600

    prompt to delete toggle

--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,5 +1,27 @@
 <template>
-  <q-page class="flex flex-center"> Settings </q-page>
+  <q-page>
+    <div class="q-pa-md">
+      <q-list bordered padding>
+        <q-item-label header>Entries</q-item-label>
+
+        <q-item tag="label" v-ripple>
+          <q-item-section>
+            <q-item-label>Prompt to Delete</q-item-label>
+          </q-item-section>
+
+          <q-item-section side>
+            <q-toggle
+              v-model="storeSettings.settings.promptToDelete"
+            ></q-toggle>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStoreSettings } from 'src/stores/storeSettings'
+
+const storeSettings = useStoreSettings()
+</script>
